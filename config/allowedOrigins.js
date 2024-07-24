d2f6df33f20fd788c8f6811a181af58c562c720c
Khushi Raj <121477819+KhushiRaj23@users.noreply.github.com>
2024-07-25 01:42:18 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://khushi-blogging-app.netlify.app/'
+    'https://khushi-blogging-app.netlify.app'
 
 ];
 
